a16423bc8c544ff392ce17f439d14963e4dbce7d
Mo1598 <ngugipious1598@gmail.com>
2021-07-01 08:54:25 +0300

    Second Commit

--- app/Http/Controllers/EmployeesController.php
+++ app/Http/Controllers/EmployeesController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Excel;
 use App\Imports\EmployeeImport;
+use App\Models\Employee;
+use Illuminate\Support\Facades\Session;
 
 class EmployeesController extends Controller
 {
@@ -12,7 +14,7 @@ class EmployeesController extends Controller
     {
         return view('import-form');
     }
-    public function import(Request $request)
+    /* public function import(Request $request)
     {
           $data = request()->validate([
             'file'=>'required|mimes:xls,csv,xlsx'
@@ -20,6 +22,21 @@ class EmployeesController extends Controller
 
         Excel::import(new EmployeeImport, request()->file);
         return back()->with('success', 'Your details were imported successfully!!!');
-    }
+    } */
+    function import(Request $request){
+        $this->validate($request,[
+           'file'=>'required|mimes:csv,xlsx'
+        ]);
+        $file=file($request->file->getRealPath());
+        $data=array_slice($file,1);# remove the fist line of the data
+        $parts=(array_chunk($data,200));#cut files into parts for php timeout
+        foreach($parts as $index=> $part){
+           $fileName=resource_path('pending-uploads/'.date('y-m-d-H-i-s').$index.'.csv');
+           file_put_contents($fileName,$part);
+        }
+        (new Employee())->importToDB();
+        Session::flash('success','Your file has been uploaded successfully.');
+        return redirect()->back();
+     }
     
 }
--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -12,9 +12,33 @@ class Employee extends Model
     protected $table = "employees";
     protected $fillable = ['name', 'email', 'phone', 'salary', 'department'];
 
-    public static function getEmployee()
+    /* public static function getEmployee()
     {
         $records=Employee::select('id', 'name', 'email', 'phone', 'salary', 'department');
         return $records;
+    } */
+    public function importToDB(){
+        $path=resource_path('pending-files/*.csv');
+        $g=glob($path);
+       
+        foreach(array_slice($g,0,1) as $file){
+            $data=array_map('str_getcsv',file($file));
+            
+            foreach($data as $row){
+               
+                // self::updateOrCreate([
+
+                // ]);
+                self::updateOrCreate([
+                    'name'=>$row['0'],
+                    'emmail'=>$row['1'],
+                    'phone'=>$row['2'],
+                    'salary'=>$row['3'],
+                    'department'=>$row['4']
+                ]);
+            }
+            unlink($file);
+        }
+        return;
     }
 }
